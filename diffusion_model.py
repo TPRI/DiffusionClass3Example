1f365a543ead31c93948efd042599713d5bb0755
TPRI <timothy.rose-innes.14@ucl.ac.uk>
2014-10-19 10:49:24 +0100

    Second attempt refering to solution

--- diffusion_model.py
+++ diffusion_model.py
@@ -10,10 +10,22 @@ def energy(density, coeff=1.0):
           Diffusion coefficient.
   """
   
-  #Initialise e (before using  e +=)
-  e = 0
+  # #Initialise e (before using  e +=)
+  # e = 0
   
-  #loop over all values in array
-  for n in density:
-    e += (coeff / 2) * n * (n + 1)
-  return e
+  # #loop over all values in array
+  # for n in density:
+    # e += (coeff / 2) * n * (n + 1)
+  # return e
+
+  #Improved attempt
+  
+  #Imports
+  from numpy import array, sum
+  
+  #Assign density list to an numpy array
+  density = array(density)
+  
+  #Main code in return 
+  return coeff * 0.5 * sum(density * (density - 1))
+  
